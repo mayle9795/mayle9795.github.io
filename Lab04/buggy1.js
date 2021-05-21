308e0796bfe1382887bfc1441959ab79f5c7dc3b
mayle <mayle9795@gmail.com>
2021-05-21 10:59:17 -0500

    Done fixxing bugs

---
+++ Lab04/buggy1.js
@@ -0,0 +1,6 @@
+function gogogo() {
+    alert("Yay, it works!");
+}
+//The code ia right. The fact that is JavaScript programs have no main. They respond to user actions called events. User actions can call event from html file
+//Solution: Create an html file to interact with user then this Javascript code can work when getting request fromm user
+//Solution in buggy1.html
